06eb045d3b184ac327a270c44760c8fdbb1ce300
Hassan Farooq <hassan@pop-os.localdomain>
2019-11-02 14:16:46 -0500

    Updated site

--- draw_site/run_script.php
+++ draw_site/run_script.php
@@ -1,16 +1,9 @@
 <?php
-ini_set('set_time_limit', 300000);
-	$dir = '/var/www/html/files';
-	if(!file_exists($dir)) {
-		mkdir($dir, 0744);
-	}
-
-  $command = escapeshellcmd('/usr/bin/python3 /var/www/html/test.py');
-//	sleep(10);
-$output = shell_exec($command);
-echo "test";
-  echo $output;
-echo "test2";
+	$inputVal = $_GET["myCountry"];
+	//echo $inputVal;
+	$command = escapeshellcmd('/usr/bin/python3 /var/www/html/test.py ' . $inputVal);
+	$output = shell_exec($command);
+	echo $output;
 	file_put_contents('/var/www/html/files/output.txt', $output);
- header('Location: http://23.99.220.51/displayUserGraph.html?success=true');
+	header('Location: results.html?success=true');
 ?>
